d00919bf5dac99680a9a636187d096c80b57c121
Bumbu Nasi Goreng Sasa <yuttaputri@student.undip.ac.id>
2019-01-03 21:41:50 +0700

    2nd Commit
    UI init

--- app/src/main/java/com/example/yuttanandiyaputri/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/yuttanandiyaputri/tictactoe/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.yuttanandiyaputri.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button[][] buttons = new Button[3][3];
+    private boolean player1Turn = true;
+    private int roundCount;
+    private int player1Points;
+    private int player2Points;
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewPlayer1.findViewById(R.id.text_view_p1);
+        textViewPlayer2.findViewById(R.id.text_view_p2);
+
+        for(int i = 0; i<3; i++){
+
+        }
     }
 }
